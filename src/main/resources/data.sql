276c894bef9e342b0bc716f3e5d8c6371b9625e5
marek.vu <marekvu1@email.cz>
2024-04-15 14:00:20 +0200

    [NF] new feature added testData

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.link_entity
+(available_in_chrome, available_in_firefox, deleted, description, image, image_base64, is_active, "name", open_in_new_window, url)
+VALUES(true, false, false, '<p>Link dostupny pouze v chromu, otevre se v novem okne</p>', 17821, NULL, true, 'Link dostupny pouze v chromu, otevre se v novem okne', true, 'www.google.com');
+INSERT INTO public.link_entity
+(available_in_chrome, available_in_firefox, deleted, description, image, image_base64, is_active, "name", open_in_new_window, url)
+VALUES(true, false, false, '<p>Link dostupny pouze v chromu, neotevre se v novem okne</p>', 17823, NULL, true, 'Link dostupny pouze v chromu, neotevre se v novem okne', false, 'www.google.com');
+INSERT INTO public.link_entity
+(available_in_chrome, available_in_firefox, deleted, description, image, image_base64, is_active, "name", open_in_new_window, url)
+VALUES(false, true, false, '<p>Link dostupny pouze v firefoxu, otevre se v novem okne</p>', 17827, NULL, true, 'Link dostupny pouze v firefoxu, otevre se v novem okne', true, 'www.firefox.com');
+INSERT INTO public.link_entity
+(available_in_chrome, available_in_firefox, deleted, description, image, image_base64, is_active, "name", open_in_new_window, url)
+VALUES(false, true, false, '<p><em><u>Link dostupny pouze v firefoxu, neotevre se v novem okne</u></em></p>', 17830, NULL, true, 'Link dostupny pouze v firefoxu, neotevre se v novem okne', false, 'www.firefox.com');
